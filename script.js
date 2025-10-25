bd3649cbc6b9c3d5b355c5bbe4be93f00ad88af4
Amritraj96 <amritraj.up.yadav@gmail.com>
2025-10-25 19:00:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,11 +40,12 @@ function removeAdmin() {
 function concatenateArray() {
   //Write your code here, just console.log
   const newArr = [
-    { id: 4, name: "tom", age: "22", profession: "designer" },
-    { id: 5, name: "lisa", age: "23", profession: "manager" },
-    { id: 6, name: "sam", age: "24", profession: "developer" },
+    { id: 4, name: "Ram", age: "22", profession: "designer" },
+    { id: 5, name: "Sita", age: "23", profession: "manager" },
+    { id: 6, name: "Lakshman", age: "24", profession: "developer" },
   ];
 
   const combined = arr.concat(newArr);
   console.log(combined);
+
 }
